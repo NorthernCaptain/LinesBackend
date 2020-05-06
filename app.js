d2b0f98f4fdd27cc667eecfde7c2790a1de3869f
Leonid Khramov <leonidtheking@gmail.com>
2020-05-06 08:51:17 -0500

    added redirect from http to https

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const app = express();
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(express.static('public'));
+//redirect all http traffic to https
+app.use(function(req, res, next) {
+    if(!req.secure) {
+        return res.redirect(['https://', req.get('Host'), req.baseUrl].join(''));
+    }
+    next();
+});
 
 const privateKey  = fs.readFileSync('etc/letsencrypt/live/lines.navalclash.com/privkey.pem', 'utf8');
 const certificate = fs.readFileSync('etc/letsencrypt/live/lines.navalclash.com/fullchain.pem', 'utf8');
@@ -18,7 +25,7 @@ httpsServer.listen(8443, '0.0.0.0', (err) => {
         if (err) {
             console.error("ERROR: ", err);
         }
-
-        process.setuid(65534); //nobody
-        console.log('Server\'s UID is now ' + process.getuid());
+        console.log('Server started, UID is now ' + process.getuid());
 });
+
+app.listen(8080, '0.0.0.0');
